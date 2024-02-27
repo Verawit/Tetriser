bf56453a71960a06ba47facb2e6cb446e649dff6
Verawit <Verawit2548@gmail.com>
2024-02-27 22:29:08 +0700

    แก้บัค2 + gameover

--- main.cpp
+++ main.cpp
@@ -44,7 +44,7 @@ int main()
 {
     srand(time(0));
 
-    RenderWindow window(VideoMode(320, 480), "Tetris");
+    RenderWindow window(VideoMode(400, 480), "TetrisTetruy");
 
     Texture t1, t2, t3;
     t1.loadFromFile("images/tiles.png");
@@ -57,6 +57,7 @@ int main()
     bool rotate = 0;
     int colorNum = 1 + rand() % 7;
     float timer = 0, delay = 0.3;
+    bool gameOver = false;
 
     Clock clock;
 
@@ -69,134 +70,143 @@ int main()
 
     while (window.isOpen())
     {
-        float time = clock.getElapsedTime().asSeconds();
-        clock.restart();
-        timer += time;
+        if(gameOver == false){
+            float time = clock.getElapsedTime().asSeconds();
+            clock.restart();
+            timer += time;
 
-        Event e;
-        while (window.pollEvent(e))
-        {
-            if (e.type == Event::Closed) {
-                window.close();
+            Event e;
+            while (window.pollEvent(e))
+            {
+                if (e.type == Event::Closed) {
+                    window.close();
+                }
+
+                if (e.type == Event::KeyPressed)
+                    if (e.key.code == Keyboard::Up) {
+                        rotate = true;
+                    }
+                    else if (e.key.code == Keyboard::Left) {
+                        xPos = -1;
+                    }
+                    else if (e.key.code == Keyboard::Right) {
+                        xPos = 1;
+                    }
             }
 
-            if (e.type == Event::KeyPressed)
-                if (e.key.code == Keyboard::Up) {
-                    rotate = true;
-                }
-                else if (e.key.code == Keyboard::Left) {
-                    xPos = -1;
-                }
-                else if (e.key.code == Keyboard::Right) {
-                    xPos = 1;
-                }
-        }
+            if (Keyboard::isKeyPressed(Keyboard::Down)) {
+                delay = 0.05;
+            }
 
-        if (Keyboard::isKeyPressed(Keyboard::Down)) {
-            delay = 0.05;
-        }
-
-        //ขยับ
-        for (int i = 0;i < 4;i++) {
-            b[i] = a[i];
-            a[i].x += xPos; 
-        }
-        if (checkCollision() == true) {
+            //ขยับ
             for (int i = 0;i < 4;i++) {
-                a[i] = b[i];
-            }
-        }
-        //หมุน
-        if (rotate) {
-            Point center = a[1]; //center of rotation
-            for (int i = 0; i < 4; i++) {
-                int x = a[i].y - center.y;
-                int y = a[i].x - center.x;
-                a[i].x = center.x - x;
-                a[i].y = center.y + y;
+                b[i] = a[i];
+                a[i].x += xPos;
             }
             if (checkCollision() == true) {
                 for (int i = 0;i < 4;i++) {
                     a[i] = b[i];
                 }
             }
-        }
-
-        //Tick
-        if (timer > delay) {
-            for (int i = 0;i < 4;i++) { 
-                b[i] = a[i]; 
-                a[i].y += 1; 
-            }
-
-            if (checkCollision() == true) {
+            //หมุน
+            if (rotate) {
+                Point center = a[1]; //center of rotation
                 for (int i = 0; i < 4; i++) {
-                    field[b[i].y][b[i].x] = colorNum;
+                    int x = a[i].y - center.y;
+                    int y = a[i].x - center.x;
+                    a[i].x = center.x - x;
+                    a[i].y = center.y + y;
                 }
+                if (checkCollision() == true) {
+                    for (int i = 0;i < 4;i++) {
+                        a[i] = b[i];
+                    }
+                }
+            }
 
-              
-                colorNum = 1 + rand() % 7;
-                int n = rand() % 7;
+            //Tick
+            if (timer > delay) {
                 for (int i = 0;i < 4;i++) {
-                    a[i].x = block[n][i] % 2;
-                    a[i].y = block[n][i] / 2;
+                    b[i] = a[i];
+                    a[i].y += 1;
                 }
-                for (int i = 0;i < 4;i++) {
-                    a[i].x += 4;
+
+                if (checkCollision() == true) {
+                    for (int i = 0; i < 4; i++) {
+                        field[b[i].y][b[i].x] = colorNum;
+                    }
+
+
+                    colorNum = 1 + rand() % 7;
+                    int n = rand() % 7;
+                    for (int i = 0;i < 4;i++) {
+                        a[i].x = block[n][i] % 2;
+                        a[i].y = block[n][i] / 2;
+                    }
+                    for (int i = 0;i < 4;i++) {
+                        a[i].x += 4;
+                    }
+
+                    //gameover
+                    for (int i = 0; i < 4; i++) {
+                        if (field[a[i].y][a[i].x] != 0) {
+                            std::cout << "GAMEOVER ";
+                            gameOver = true;
+
+                        }
+                    }
+
+                }
+
+                timer = 0;
+            }
+
+            //เช็คแถว
+            int k = M - 1;
+            for (int i = M - 1;i > 0;i--) {
+                int count = 0;
+                for (int j = 0;j < N;j++) {
+                    if (field[i][j]) {
+                        count++;
+                    }
+                    field[k][j] = field[i][j];
+                }
+                if (count < N) {
+                    k--;
                 }
             }
 
-            timer = 0;
-        }
+            xPos = 0;
+            rotate = 0;
+            delay = 0.3;
 
-        //เช็คแถว
-        int k = M - 1;
-        for (int i = M - 1;i > 0;i--) {
-            int count = 0;
-            for (int j = 0;j < N;j++) {
-                if (field[i][j]) {
-                    count++;
+            //วาดพื่นหลัง
+            window.clear(Color::White);
+            window.draw(background);
+
+            for (int i = 0;i < M;i++) {
+                for (int j = 0;j < N;j++) {
+                    if (field[i][j] == 0) {
+                        continue;
+                    }
+                    s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+                    s.setPosition(j * 18, i * 18);
+                    s.move(28, 31); //offset
+                    window.draw(s);
                 }
-                field[k][j] = field[i][j];
             }
-            if (count < N) {
-                k--;
-            }
-        }
 
-        xPos = 0; 
-        rotate = 0; 
-        delay = 0.3;
-
-        //check gameover
-        
-
-        //วาดพื่นหลัง
-        window.clear(Color::White);
-        window.draw(background);
-
-        for (int i = 0;i < M;i++) {
-            for (int j = 0;j < N;j++) {
-                if (field[i][j] == 0) {
-                    continue;
-                }
-                s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
-                s.setPosition(j * 18, i * 18);
+            for (int i = 0; i < 4; i++) {
+                s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
+                s.setPosition(a[i].x * 18, a[i].y * 18);
                 s.move(28, 31); //offset
                 window.draw(s);
             }
-        }
-       
-        for (int i = 0; i < 4; i++) {
-            s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
-            s.setPosition(a[i].x * 18, a[i].y * 18);
-            s.move(28, 31); //offset
-            window.draw(s);
-        }
 
-        window.draw(frame);
-        window.display();
+            window.draw(frame);
+            window.display();
+        }
     }
-
+    
     return 0;
 }
