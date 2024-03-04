3f66a3dd8ccd19f00e5cad21965c344a36f3f4d9
Verawit <Verawit2548@gmail.com>
2024-03-05 05:02:06 +0700

    hdgakasdhjkgfhsdkgfhdkghjgfsdkghfsdfhsdga

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <time.h>
+#include <string>
 #include <iostream>
+#include <cmath>
 
 using namespace sf;
 
@@ -46,17 +49,56 @@ int main()
 
     RenderWindow window(VideoMode(400, 480), "TetrisTetruy");
 
-    Texture t1, t2, t3;
-    t1.loadFromFile("images/tiles.png");
-    t2.loadFromFile("images/background.png");
-    t3.loadFromFile("images/frame.png");
+    Music music;
 
-    Sprite s(t1), background(t2), frame(t3);
+    SoundBuffer scoreup;
+    Sound scoreup_sound;
+    scoreup_sound.setBuffer(scoreup);
+
+    SoundBuffer over;
+    Sound over_sound;
+    over_sound.setBuffer(over); 
+
+
+    if (!music.openFromFile("audio/bgmm.wav")) {
+        std::cout << "ERROR";
+    }
+
+    if (!scoreup.loadFromFile("audio/scoreup.wav")) {
+        std::cout << "ERROR";
+    }
+
+    if (!over.loadFromFile("audio/gameover.wav")) {
+        std::cout << "ERROR";
+    }
+
+    
+    music.play();
+    music.setVolume(70);
+    
+    Texture texture1, texture2, texture3, texture4, texture5, texture6;
+    texture1.loadFromFile("images/tiles.png");
+    texture2.loadFromFile("images/background.png");
+    texture3.loadFromFile("images/frame.png");
+    texture4.loadFromFile("images/numbers.png");
+    texture5.loadFromFile("images/gameover.png");
+    texture6.loadFromFile("images/finalscore.png");
+
+    Sprite s(texture1), background(texture2), frame(texture3), nums(texture4), gameover(texture5), scorefinal(texture6);
+
+    int score = 0;
+    int scoreDigits;
+    int number[10] = { 0,1,2,3,4,5,6,7,8,9 };
+
+    int comboCount = 0;
+    int comboMultiplier = 1;
+    float timeSinceLastClear = 0.0f;
+    const float comboWindow = 2.0f;
 
     int xPos = 4; 
     bool rotate = 0;
     int colorNum = 1 + rand() % 7;
-    float timer = 0, delay = 0.3;
+    float timer = 0, delay = 0.01;
     bool gameOver = false;
 
     Clock clock;
@@ -70,34 +112,64 @@ int main()
 
     while (window.isOpen())
     {
-        if(gameOver == false){
+                
             float time = clock.getElapsedTime().asSeconds();
             clock.restart();
             timer += time;
-
             Event e;
             while (window.pollEvent(e))
             {
                 if (e.type == Event::Closed) {
                     window.close();
                 }
+                if (e.type == Event::KeyPressed) {
+                    if (e.key.code == Keyboard::R) {
+                        std::cout << "restart";
+                        score = 0;
+                        comboCount = 0;
+                        comboMultiplier = 1;
+                        timeSinceLastClear = 0.0f;
+                        gameOver = false;
 
-                if (e.type == Event::KeyPressed)
-                    if (e.key.code == Keyboard::Up) {
-                        rotate = true;
+                        // Clear the game grid
+                        for (int i = 0; i < M; i++) {
+                            for (int j = 0; j < N; j++) {
+                                field[i][j] = 0;
+                            }
+                        }
+
+                        // Set up a new Tetrimino piece
+                        colorNum = 1 + rand() % 7;
+                        n = rand() % 7;
+                        for (int i = 0; i < 4; i++) {
+                            a[i].x = block[n][i] % 2;
+                            a[i].y = block[n][i] / 2;
+                        }
+                        for (int i = 0; i < 4; i++) {
+                            a[i].x += 4;
+                        }
                     }
-                    else if (e.key.code == Keyboard::Left) {
-                        xPos = -1;
-                    }
-                    else if (e.key.code == Keyboard::Right) {
-                        xPos = 1;
-                    }
+                }
+                if (gameOver == false) {
+                    if (e.type == Event::KeyPressed)
+                        if (e.key.code == Keyboard::Up) {
+                            rotate = true;
+                        }
+                        else if (e.key.code == Keyboard::Left) {
+                            xPos = -1;
+                        }
+                        else if (e.key.code == Keyboard::Right) {
+                            xPos = 1;
+                        }
+                }
             }
+
 
             if (Keyboard::isKeyPressed(Keyboard::Down)) {
                 delay = 0.05;
             }
 
+        if (gameOver == false) {
             //ขยับ
             for (int i = 0;i < 4;i++) {
                 b[i] = a[i];
@@ -138,7 +210,7 @@ int main()
 
 
                     colorNum = 1 + rand() % 7;
-                    int n = rand() % 7;
+                    n = rand() % 7;
                     for (int i = 0;i < 4;i++) {
                         a[i].x = block[n][i] % 2;
                         a[i].y = block[n][i] / 2;
@@ -151,8 +223,8 @@ int main()
                     for (int i = 0; i < 4; i++) {
                         if (field[a[i].y][a[i].x] != 0) {
                             std::cout << "GAMEOVER ";
+                            over_sound.play();
                             gameOver = true;
-
                         }
                     }
 
@@ -173,7 +245,31 @@ int main()
                 }
                 if (count < N) {
                     k--;
+
                 }
+                else {
+                    //นับคะแนน
+                    scoreup_sound.play();
+                    score += 100 * comboMultiplier;
+                    comboCount++;
+                    if (comboCount > 1) {
+                        comboMultiplier++;
+                    }
+
+                    
+                    std::string scoreStr = std::to_string(score);
+
+                    timeSinceLastClear = 0.0f;
+                    std::cout << score << std::endl;
+                    
+                }
+            }
+
+            timeSinceLastClear += time;
+
+            if (timeSinceLastClear > comboWindow) {
+                comboCount = 0;
+                comboMultiplier = 1;
             }
 
             xPos = 0;
@@ -183,6 +279,17 @@ int main()
             //วาดพื่นหลัง
             window.clear(Color::White);
             window.draw(background);
+
+            scoreDigits = (log10(score) + 1);
+
+            std::string scoreStr = std::to_string(score);
+
+            for (int i = 0; i < scoreDigits; i++) {
+                nums.setPosition(320, 80);
+                nums.setTextureRect(IntRect((int(scoreStr[scoreStr.length() - i - 1]) - 48) * 18, 0, 18, 18));
+                nums.move(i * -18, 0);
+                window.draw(nums);
+            }
 
             for (int i = 0;i < M;i++) {
                 for (int j = 0;j < N;j++) {
@@ -196,6 +303,8 @@ int main()
                 }
             }
 
+
+            //วาดรูปบล็อกตามสีตอนถึงพื่น
             for (int i = 0; i < 4; i++) {
                 s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
                 s.setPosition(a[i].x * 18, a[i].y * 18);
@@ -203,6 +312,24 @@ int main()
                 window.draw(s);
             }
 
+            if (gameOver) {
+                window.clear(Color::White);
+                window.draw(background);
+                window.draw(frame);
+                gameover.setPosition(30, 100);
+                window.draw(gameover);
+                scorefinal.setPosition(30, 150);
+                window.draw(scorefinal);
+                for (int i = 0; i < scoreDigits; i++) {
+                    nums.setPosition(125, 200);
+                    nums.setTextureRect(IntRect((int(scoreStr[scoreStr.length() - i - 1]) - 48) * 18, 0, 18, 18));
+                    nums.move(i * -18, 0);
+                    window.draw(nums);
+                }
+                window.display();
+                continue;
+            }
+            
             window.draw(frame);
             window.display();
         }
